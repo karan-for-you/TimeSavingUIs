f6742463fed4712563f384842e2f4c7ee14f913a
karan <karanjeet.singh@oodlestechnologies.com>
2020-07-20 18:32:43 +0530

    Clean up

--- app/src/main/java/com/example/myapplication/stickyheaderlibrary/Country.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.stickyheaderlibrary
-
-import com.saber.stickyheader.stickyData.StickyMainData
-
-data class Country(
-    var name: String,
-    var countryCode: String
-) :  StickyMainData
